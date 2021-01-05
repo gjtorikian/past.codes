35fad0d1002492ca11f7b1aecb0f150c8e70bac2
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2021-01-05 15:01:37 +0000

    Memoize current_email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+
+  helper_method :current_user, :current_email
 
   def render404
     render 'error/404', status: :not_found
@@ -14,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(github_id: session[:uid])
+  end
+
+  def current_email
+    @current_email ||= GitHubClient.new(current_user.encrypted_github_token).fetch_primary_email_and_scopes[:primary_email]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
   validates :github_id, :frequency, :github_username, :encrypted_github_token, presence: true
   enum frequency: { weekly: 0, monthly: 1 }
 
-  def email_address
-    GitHubClient.new(encrypted_github_token).fetch_primary_email_and_scopes[:primary_email]
-  end
-
   def self.find_or_create!(authorize_params, encrypted_github_token)
     user = find_by(github_id: authorize_params.fetch('uid'))
 
